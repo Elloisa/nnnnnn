3ebefc55dc6e016a6d6709f501a2007643f50439
Lorena Tito <lorena.tito.ramos@gmail.com>
2021-01-13 20:28:13 -0500

    Se ha colocado home a la ruta

--- appMercadoLiebre.js
+++ appMercadoLiebre.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-app.get("/home", (req, res) => {
-    res.send("Hola Mundo!");
-});
+
+const publicFolderPath = path.resolve(__dirname, "./mercadoLiebreDH/public");
+app.use(express.static(publicFolderPath));
+
 app.listen(3003, () => {
     console.log("El servidor 3000 está corriendo");
+});
+
+app.get("/home", (req, res) => {
+    let htmlPath= path.resolve(__dirname, "./mercadoLiebreDH/views/homeMercadoLiebre.html");
+    res.sendFile(htmlPath);
 })
