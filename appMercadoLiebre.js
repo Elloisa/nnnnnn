6d5225d8b7ff7260259d6fac13749747443f0252
Lorena Tito <lorena.tito.ramos@gmail.com>
2021-01-13 22:18:58 -0500

    Se ha corregido la carga del logo

--- appMercadoLiebre.js
+++ appMercadoLiebre.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 
-const publicFolderPath = path.resolve(__dirname, "./mercadoLiebreDH/public");
+const publicFolderPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicFolderPath));
 
 app.listen(3003, () => {
